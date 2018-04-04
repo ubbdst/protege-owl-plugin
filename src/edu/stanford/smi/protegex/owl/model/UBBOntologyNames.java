446c8d1b0c08edf59604f73375b3d8f863015a0a
Hemed Ali <hru066@ubbhf0225453.klientdrift.uib.no>
2018-04-04 14:46:16 +0200

    Use momayo namespaces for UUID and previousIdentifier

--- src/edu/stanford/smi/protegex/owl/model/UBBOntologyNames.java
+++ src/edu/stanford/smi/protegex/owl/model/UBBOntologyNames.java
@@ -5,23 +5,21 @@ package edu.stanford.smi.protegex.owl.model;
  *         University of Bergen Library
  *         2017-04-28
  *
- * I think this is a dirty way to access namespaces,
- * we should have a mechanism to automatically retrive these from the active ontology.
+ * I think this is not an elegent way to access namespaces,
+ * we should have a mechanism to automatically retrieve these from the active ontology.
  */
 public class UBBOntologyNames {
 
     //Default UBB namespace
     public static final String DEFAULT_NAMESPACE = "http://data.ub.uib.no/";
-
-    //Namespace for UBB ontology
     public static final String UBBONT_NAMESPACE =     "http://data.ub.uib.no/ontology/";
+    public static final String MOMAYO_NAMESPACE =     "http://purl.org/momayo/mmo/";
 
-    public static final String UUID =                  UBBONT_NAMESPACE + "uuid";
+    public static final String UUID =                  MOMAYO_NAMESPACE + "uuid";
+    public static final String PREVIOUS_IDENTIFIER =   MOMAYO_NAMESPACE + "previousIdentifier";
     public static final String TRASH_CLASS_NAME =      UBBONT_NAMESPACE + "Trash";
     public static final String CLASS_HIERARCHY_URI =   UBBONT_NAMESPACE + "classHierarchyURI";
-    public static final String PREVIOUS_IDENTIFIER =   UBBONT_NAMESPACE + "previousIdentifier";
     public static final String HAS_BEEN_MERGED_WITH =  UBBONT_NAMESPACE + "hasBeenMergedWith";
-
     public static final String TITLE =                 "http://purl.org/dc/terms/title";
     public static final String IDENTIFIER  =           "http://purl.org/dc/terms/identifier";
 
